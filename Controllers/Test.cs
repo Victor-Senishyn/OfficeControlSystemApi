f6c7a81f2ec2e85124de75d743a202c0ba30e373
Victor-Senishyn <senishin2004@gmail.com>
2024-03-23 22:11:15 +0200

    Added test controller

---
+++ Controllers/Test.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OfficeControlSystemApi.Models.DTOs;
+using OfficeControlSystemApi.Models.Enums;
+
+namespace OfficeControlSystemApi.Controllers
+{
+    public class Test : Controller
+    {
+        [HttpPost("/test")]
+        public async Task<IActionResult> Test1() => Ok("Hello world");
+    }
+}
